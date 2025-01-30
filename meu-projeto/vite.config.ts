0e001b0f23cd6a07657eccb63a6635c5bd965b7b
BernardoRibeiro18 <bernardozbr@hotmail.com>
2025-01-29 23:57:42 -0300

    _redirects

--- meu-projeto/vite.config.ts
+++ meu-projeto/vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "./", // Garante que os assets carreguem corretamente
+  build: {
+    outDir: "dist",
+  },
+});

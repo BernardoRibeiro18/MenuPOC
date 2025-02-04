679920a25e4492c9d3e05bbea38d584e8adae720
BernardoRibeiro18 <bernardozbr@hotmail.com>
2025-02-04 19:38:52 -0300

    new Observatorio description

--- meu-projeto/src/pages/Observatorio.tsx
+++ meu-projeto/src/pages/Observatorio.tsx
@@ -5,7 +5,7 @@ const Observatorio: React.FC = () => {
     <div>
       <h1>Bem-vindo à página Observatorio!</h1>
       <p>
-        O Observatório Ambiental da Mata é uma Iniciativa em rede, vinculado ao Museu Parque Usina Maurício e ao Instituto Energisa, com a missão de sistematizar e divulgar de forma clara e acessível ao público em geral informações e dados sobre o bioma da Mata Atlântica na zona da Mata Mineira, estimulando a reflexão, a ação coletiva  e a cooperação territorial, articulando-se em três pilares: compreender, observar e proteger.
+      O Observatório Cultual da Mata é uma Iniciativa em rede, vinculado a Casa da Memória de Leopoldina e a Fundação Cultural Ormeo Junqueira Botelho, com a missão de sistematizar e divulgar de forma clara e acessível ao público em geral informações e dados sobre a cultura na zona da Mata Mineira, estimulando a reflexão, a ação coletiva  e a cooperação territorial, articulando-se em três pilares: compreender, observar e proteger.
       </p>
     </div>
   )
